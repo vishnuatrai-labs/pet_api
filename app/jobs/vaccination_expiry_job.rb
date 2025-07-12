a7c5b9fcf7c9e868acae3c6968118cae63a6e063
Vishnu Atrai <vishnu.atrai+1@gmail.com>
2025-07-12 10:43:01 +0400

    perform job

--- app/jobs/vaccination_expiry_job.rb
+++ app/jobs/vaccination_expiry_job.rb
@@ -1,7 +1,9 @@
 class VaccinationExpiryJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(pet_id)
+    pet = Pet.find(pet_id)
+    Rails.logger.info "Simulated email: Vaccination expired for #{pet.name} (#{pet.breed})"
+    # notification service to be integrated here
   end
 end
